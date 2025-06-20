687718fdfe1b3796029165dad345f04d2aca0b6c
Svetoslav <quicksilverx@yandex.ru>
2025-06-21 02:00:47 +0300

    2 итерация ДЗ 4 модуля, нужно код-ревью по ошибкам [нашёл фик для sendAll для Sendable]

--- src/main/java/ru/javagrind/notify/Sendable.java
+++ src/main/java/ru/javagrind/notify/Sendable.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 
 public interface Sendable {
     public abstract void send();
-}
 
-
-default void sendAll() {
-    send(); {
-        System.out.println("Отправляем сообщение на ...");
+    default void sendAll() {
+        send(); {
+            System.out.println("Отправляем сообщение на ...");
+        }
     }
+
+
 }
+
+
